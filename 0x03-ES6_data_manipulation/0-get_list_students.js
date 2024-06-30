afe41683291b86eeeae37b1295a7e5ab0bb2cbbc
saber0amine <saberamine000@gmail.com>
2024-06-30 14:25:52 +0100

    add project

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,5 @@
+export default function getListStudents() {
+    return [{ "id":1 , "firstName" : "Guillaume" , "locarion":"San Francisco"} ,
+        { "id":2 , "firstName" : "James" , "locarion":"Columbia"} , 
+        { "id":5 , "firstName" : "Serena" , "locarion":"San Francisco"} ]
+}
