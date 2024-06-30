664840627950402479ab5abfc07a652d39dcfb57
saber0amine <saberamine000@gmail.com>
2024-06-30 14:40:18 +0100

    add 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+function getListStudentIds(arrayObject) {
+    if(!Array.isArray(arrayObject)) {
+        return new Array() ; 
+    }
+    else {
+        return arrayObject.map((obj) => obj.id) ; 
+    }
+
+}
